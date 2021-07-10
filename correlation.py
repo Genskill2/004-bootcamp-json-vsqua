aa51d97a92c1d565c482584d2f0b7d905178ff1b
vsqua <84577034+vsqua@users.noreply.github.com>
2021-07-10 21:36:50 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -3,9 +3,9 @@ import json
 import math
 
 def load_journal(file_name):
-    f = open(file_name,'q')
-    read_data = f.read()
-    list_of_dicts = json.loads(read_data)
+    file1= open(file_name,'r')
+    read_data=file1.read()
+    list_of_dicts=json.loads(read_data)
     return list_of_dicts
 
 def compute_phi(file_name,event):
@@ -28,7 +28,7 @@ def compute_phi(file_name,event):
             squirrel_false = squirrel_false+1
 
         if event in i["events"]:
-            event_true = event_true+1
+            event_true=event_true+1
             if i["squirrel"]:
                 event_true_squirrel_true = event_true_squirrel_true+1
             else:    
@@ -41,32 +41,32 @@ def compute_phi(file_name,event):
             else:    
                 event_false_squirrel_false = event_false_squirrel_false+1
 
-    correlation = ((event_true_squirrel_true*event_false_squirrel_false) - (event_true_squirrel_false*event_false_squirrel_true))/math.sqrt(event_true*event_false*squirrel_true*squirrel_false)
+    correlation=((event_true_squirrel_true*event_false_squirrel_false) - (event_true_squirrel_false*event_false_squirrel_true))/math.sqrt(event_true*event_false*squirrel_true*squirrel_false)
     return correlation
 
 def compute_correlations(file_name):
-    list1 = load_journal(file_name)
-    events = []
-    dict = {}
+    list1=load_journal(file_name)
+    events=[]
+    dict={}
     for i in list1:
-        list2 = i["events"]
+        list2=i["events"]
         for j in list2:
             if j not in events: 
                 events.append(j)
-                correlation = compute_phi(file_name,j)
-                dict[j] = correlation
+                correlation=compute_phi(file_name,j)
+                dict[j]=correlation
      
     return dict
 
 def diagnose(file_name):
     
-    dict = compute_correlations(file_name)
-    pos = str(list(dict.keys())[0])
-    neg = str(list(dict.keys())[0])
+    dict=compute_correlations(file_name)
+    pos=str(list(dict.keys())[0])
+    neg=str(list(dict.keys())[0])
     for (key,val) in dict.items():
         if val>0 and val >dict[pos]:
-            pos = str(key)
+            pos= str(key)
         if val<0 and val<dict[neg]:
-            neg = str(key)
+            neg=str(key)
 
     return pos,neg
